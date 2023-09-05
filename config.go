2816e7fa4027a017b01e8b5b14940e219e63383e
Tsumian Tsng <llbmelon@gmail.com>
2023-09-05 23:50:07 +0800

    使用goroutine进行递归生成，后续更新readme.md以及添加使用说明。

--- config.go
+++ config.go
@@ -1,22 +1,24 @@
 package main
 
 type Config struct {
-	JsonFile       string `yaml:"json-file"`
-	TemplateFile   string `yaml:"template-file"`
-	DefaultType    string `yaml:"default-type"`
-	ClassName      string `yaml:"class-name"`
-	FileOutputFlag bool   `yaml:"file-output-flag"`
-	PauseFlag      bool   `yaml:"pause-flag"`
+	JsonFile            string `yaml:"json-file"`
+	TemplateFile        string `yaml:"template-file"`
+	DefaultType         string `yaml:"default-type"`
+	RootClassName       string `yaml:"root-class-name"`
+	FileOutputFlag      bool   `yaml:"file-output-flag"`
+	FileOutputDirectory string `yaml:"file-output-directory"`
+	PauseFlag           bool   `yaml:"pause-flag"`
 }
 
 // Initialize a Config struct
 func newConfig() *Config {
 	return &Config{
-		JsonFile:       "sample.json",
-		TemplateFile:   "template.txt",
-		DefaultType:    "String",
-		ClassName:      "MyClass",
-		FileOutputFlag: false,
-		PauseFlag:      true,
+		JsonFile:            "sample.json",
+		TemplateFile:        "template.txt",
+		DefaultType:         "String",
+		RootClassName:       "MyClass",
+		FileOutputFlag:      false,
+		FileOutputDirectory: "output",
+		PauseFlag:           true,
 	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"text/template"
 
 	"gopkg.in/yaml.v3"
@@ -17,25 +19,30 @@ type Class struct {
 }
 
 var config *Config
+var wg sync.WaitGroup
+var outputDir string
 
 func main() {
 	getConfig()
 
-	// Read the JSON file
-	properties, err := getPropertyMap()
-	checkError(err)
-
-	// Define a struct for parsing the template
-	class := Class{
-		Name:       config.ClassName,
-		Properties: properties,
+	outputDir = config.FileOutputDirectory
+	if config.FileOutputFlag && len(outputDir) > 0 {
+		if f, err := os.Stat(outputDir); os.IsNotExist(err) || !f.IsDir() {
+			err := os.Mkdir(outputDir, 0700)
+			checkError(err)
+		}
+		outputDir += "/"
 	}
 
-	err = excuteOutput(class)
+	// Read the JSON file
+	data, err := getYamlData()
 	checkError(err)
+	wg.Add(1)
+	go generateClass(data, config.RootClassName)
 
+	wg.Wait()
 	if config.PauseFlag {
-		fmt.Println("\n请按回车键结束程序。")
+		fmt.Println("请按回车键结束程序。")
 		fmt.Scanln() // wait for Enter Key
 	}
 }
@@ -43,6 +50,25 @@ func main() {
 func checkError(err error) {
 	if err != nil {
 		log.Fatal("Failed to generate class definition:", err)
+	}
+}
+
+func generateClass(data map[string]interface{}, name string) {
+	defer wg.Done()
+	properties, err := getPropertyMap(data)
+	if err != nil {
+		fmt.Println(name + "类生成失败。错误信息：" + err.Error())
+		return
+	}
+
+	class := Class{
+		Name:       name,
+		Properties: properties,
+	}
+
+	err = excuteOutput(class)
+	if err != nil {
+		return
 	}
 }
 
@@ -55,7 +81,7 @@ func excuteOutput(class Class) error {
 
 	// 根据config.FileOutputFlag决定要将生成的文本输出到文件还是命令行
 	if config.FileOutputFlag {
-		outputFile := config.ClassName + ".java"
+		outputFile := outputDir + class.Name + ".java"
 		file, err := os.Create(outputFile)
 		if err != nil {
 			return err
@@ -68,18 +94,19 @@ func excuteOutput(class Class) error {
 		}
 		fmt.Println("已将生成内容输出到" + outputFile + "。")
 	} else {
-		err = tmpl.Execute(os.Stdout, class)
+		var buf bytes.Buffer
+		err = tmpl.Execute(&buf, class)
 		if err != nil {
 			return err
 		}
+		fmt.Println(buf.String())
 	}
 
 	return nil
 }
 
-// Read json file and get property map (json key -> json type)
-func getPropertyMap() (map[string]string, error) {
-	// Define the JSON string
+func getYamlData() (map[string]interface{}, error) {
+	// Get JSON string from JSON file
 	jsonString, err := os.ReadFile(config.JsonFile)
 	if err != nil {
 		return nil, fmt.Errorf("error opening file: %v", err)
@@ -92,6 +119,11 @@ func getPropertyMap() (map[string]string, error) {
 		return nil, fmt.Errorf("error parsing JSON: %v", err)
 	}
 
+	return data, nil
+}
+
+// Read json file and get property map (json key -> json type)
+func getPropertyMap(data map[string]interface{}) (map[string]string, error) {
 	// Get the keys and types
 	result := make(map[string]string)
 	for key, value := range data {
@@ -99,6 +131,20 @@ func getPropertyMap() (map[string]string, error) {
 		valueType := fmt.Sprintf("%T", value)
 		javaType := getJavaType(valueType, javaKey)
 		result[javaKey] = javaType
+
+		if valueType == "map[string]interface {}" {
+			wg.Add(1)
+			go generateClass(value.(map[string]interface{}), capitalizeFirst(javaKey))
+		}
+
+		if valueType == "[]interface {}" && len(value.([]interface{})) > 0 {
+			arrValue := value.([]interface{})
+			childType := fmt.Sprintf("%T", arrValue[0])
+			if childType == "map[string]interface {}" {
+				wg.Add(1)
+				go generateClass(arrValue[0].(map[string]interface{}), capitalizeFirst(javaKey))
+			}
+		}
 	}
 
 	return result, nil
